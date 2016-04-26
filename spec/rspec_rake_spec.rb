ddb67a5c532fb591293598b0bcb6725f08956374
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-04-27 00:43:09 +0900

    Add test for auto_reenable

--- spec/rspec_rake_spec.rb
+++ spec/rspec_rake_spec.rb
@@ -5,7 +5,16 @@ describe RSpecRake do
     expect(RSpecRake::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'auto_reenable' do
+    context 'no set' do
+      it { expect(Sample).to receive(:method_a).once }
+      after { 2.times { Rake::Task['sample:method_a'].invoke } }
+    end
+
+    context 'set auto_reenable' do
+      before { RSpecRake.auto_reenable = true }
+      it { expect(Sample).to receive(:method_b).twice }
+      after { 2.times { Rake::Task['sample:method_b'].invoke } }
+    end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,9 @@
+require 'rake'
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec_rake'
+require 'pry'
+
+Rake.application = Rake::Application.new
+task_path = File.expand_path('../tasks', __FILE__)
+Rake.application.rake_require('sample', [task_path])
+Rake::Task.define_task(:environment)
