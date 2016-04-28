e135c9260293480dee9cb41e33cfe5024f39af0e
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-04-28 09:18:16 +0900

    Use .require_tasks in spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,5 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec_rake'
 require 'pry'
 
-Rake.application = Rake::Application.new
-task_path = File.expand_path('../tasks', __FILE__)
-Rake.application.rake_require('sample', [task_path])
-Rake::Task.define_task(:environment)
+task_dir = File.expand_path('../tasks', __FILE__)
+RSpecRake.require_tasks(task_dir)
