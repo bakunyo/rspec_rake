6fd4df86d5ace7c4d90a6ffc8df2139b94455ce7
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-04-28 09:16:45 +0900

    Add .require_tasks

--- lib/rspec_rake.rb
+++ lib/rspec_rake.rb
@@ -4,5 +4,20 @@ require_relative 'rake/task'
 module RSpecRake
   class << self
     attr_accessor :auto_reenable
+
+    def require_tasks(dir)
+      Dir.glob(File.join(dir, '*.rake')).each do |file|
+        application.rake_require(File.basename(file).gsub('.rake', ''), [dir])
+      end
+      Rake::Task.define_task(:environment)
+    end
+
+    private
+
+    def application
+      @application ||= Rake::Application.new
+    end
+
+    attr_writer :application
   end
 end
