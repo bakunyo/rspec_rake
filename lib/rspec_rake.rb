226905aab7801a960a3d08f83d5a7eadbc20bac3
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-02 16:03:17 +0900

    Add .configure method to configure RSpecRake

--- lib/rspec_rake.rb
+++ lib/rspec_rake.rb
@@ -5,11 +5,16 @@ module RSpecRake
   class << self
     attr_accessor :auto_reenable
 
+    def configure
+      yield self
+
+      Rake::Task.define_task(:environment)
+    end
+
     def require_tasks(dir)
       Dir.glob(File.join(dir, '*.rake')).each do |file|
         application.rake_require(File.basename(file).gsub('.rake', ''), [dir])
       end
-      Rake::Task.define_task(:environment)
     end
 
     private
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,4 +3,6 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec_rake'
 require 'pry'
 
-RSpecRake.require_tasks(File.expand_path('../tasks', __FILE__))
+RSpecRake.configure do |config|
+  config.require_tasks(File.expand_path('../tasks', __FILE__))
+end
