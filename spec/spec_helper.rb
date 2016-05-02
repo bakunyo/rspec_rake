204cf2d014a28a9a2a91d5a971b2bd5fcf9fb7d2
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-02 15:32:58 +0900

    Refactor spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,5 +3,4 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'rspec_rake'
 require 'pry'
 
-task_dir = File.expand_path('../tasks', __FILE__)
-RSpecRake.require_tasks(task_dir)
+RSpecRake.require_tasks(File.expand_path('../tasks', __FILE__))
