c8a354c623de366e5609cd4bca1e3287e149d4f5
Izuta Hiroyuki <izuta.hiroyuki@gmail.com>
2016-05-07 17:05:20 +0900

    Add yard documents

--- lib/rspec_rake.rb
+++ lib/rspec_rake.rb
@@ -4,14 +4,25 @@ require 'rspec_rake/rake/task'
 
 module RSpecRake
   class << self
+    # Flag for method hook.  
+    # If true, call Rake::Task#reenable after Rake::Task#invoke
     attr_accessor :auto_reenable
 
+    # This method exists only to clarify that these are RSpecRake configurations.
+    # @yield [self] yield RSpecRake module instance.
+    # @example
+    #   RSpecRake.configure do |config|
+    #     config.require_tasks('rake_dir')
+    #     config.auto_reenable = true
+    #   end
     def configure
       yield self
 
       Rake::Task.define_task(:environment)
     end
 
+    # To define rake tasks, put your rake directory path
+    # @param [String] dir Directory path of rake files 
     def require_tasks(dir)
       Dir.glob(File.join(dir, '*.rake')).each do |file|
         application.rake_require(File.basename(file).gsub('.rake', ''), [dir])
